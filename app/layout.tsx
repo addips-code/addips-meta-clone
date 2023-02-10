9546f7a720d152c8174bb5ab79a5334772d07e5d
adeola adeniji <addips002@gmail.com>
2023-02-10 23:22:15 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Header from './Header'
+import { Providers } from './Providers' 
 
 export default function RootLayout({
   children,
@@ -10,7 +11,7 @@ export default function RootLayout({
     <html>
       <head />
       <body>
-        <Header/>
+        <Header session={undefined}/>
         {children}
       </body>
     </html>
